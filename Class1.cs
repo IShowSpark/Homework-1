9c4d8dbdefff3e21a6d0f8c4b2a96f9f92b20b01
Opilane TTHK <opilane@tthk.ee>
2022-02-14 10:45:32 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public static class Math
+{
+    internal static int Abs(int v)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static int Sqrt(int v)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static int Pow(int v1, int v2)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static string Round(int v1, int v2)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+namespace Homework
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(fuct.bebra(5, 5));
+
+            //fuct.arvud(2));
+
+
+
+
+            Console.OutputEncoding = Encoding.UTF8;
+            //int[] num = new int[5];
+            //int a=0;
+            //int k=0;
+            //do
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("Введи число");
+            //        a = int.Parse(Console.ReadLine());
+
+            //    }
+            //    catch (Exception exp)
+            //    {
+            //        Console.WriteLine(exp);
+            //    }
+            //    num[k] = a;
+            //    k++;
+            //} while (k < 5);
+            //int summa = 0;
+            //int korr = 1;
+            //double arif = 0;
+            //foreach (int arv in num)
+            //{
+            //    summa += arv;
+            //    korr *= arv;
+            //    arif += arv / num.Length;
+            //}
+            //Console.WriteLine($"Summa: {summa}, Korr: {korr}, Arif: {arif}");
+
+
+
+            //Random rnd = new Random();
+            //int m = rnd.Next(1, 100);
+            //int n = rnd.Next(1, 100);
+            //Console.WriteLine($"N={n}");
+            //Console.WriteLine($"M={m}");
+            //if (n > m)
+            //{
+            //    int abi = n;
+            //    n = m;
+            //    m = abi;
+            //}
+            //int[] mass = new int[m - n + 1];
+            //int j = 0;
+            //for (int i = n; i < m+1; i++)
+            //{
+            //    mass[j]= i;
+            //    j++;
+            //}
+            //foreach (int Mass in mass)
+            //{
+            //    Console.WriteLine($"{Mass*Mass,5}");
+            //}
+            //Console.WriteLine();
+
+            //char[] Letter = new char[7] { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+            //Console.WriteLine($"First letter is {Letter[0]}");
+
+            //foreach (char letter in Letter)
+            //{
+            //    Console.Write($"{letter,2}");
+            //}
+            //Random rnd = new Random();
+            //int[] number = new int[10];
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    number[i] = rnd.Next(5, 500);
+            //}
+            //foreach (int num in number)
+            //{
+            //    Console.Write($"{num,4}");
+            //}
+            //Console.WriteLine();
+            //int[,] Kino = new int[20, 10];
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        Console.WriteLine($" line {i} place {j}");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //string a = "";
+            //while (a.ToLower() != "elevant")
+            //{
+            //    Console.WriteLine("Osta elevant ära!");
+            //    a = Console.ReadLine();
+            //}
+            //Console.WriteLine("Yes");
+
+            //таблица умножения
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        Console.Write((i * j).ToString() + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //int arv;
+            //int[] arv1 = new int[4];
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine("Введи {0} число:", i + 1);
+            //    arv = int.Parse(Console.ReadLine());
+            //    arv1[i] = arv;
+            //}
+            //Array.Sort(arv1);
+
+            //int arv2 = 0;
+            //Array.Reverse(arv1);
+            //foreach (var a in arv1)
+            //{
+            //    arv2 = arv2 * 10 + a;
+            //}
+            //Console.Write(arv2);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //} while (k.Key!=ConsoleKey.Escape);
+            //Console.WriteLine("What your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("What name second person?");
+            //string name2 = Console.ReadLine();
+            //if (name == "Artjom" && name2 == "Timofei" || name == "Timofei" && name2 == "Artjom")
+            //{
+            //    Console.WriteLine($"{name} and {name2} sit together");
+            //    string otvet1 = Console.ReadLine();
+            //}
+            //else if (name == "German" && name2 == "Kevin" || name == "Kevin" && name2 == "German")
+            //{
+            //    Console.WriteLine($"{name} and {name2} sit together");
+            //    string otvet2 = Console.ReadLine();
+            //}
+            //else if (name == "Ilya" && name2 == "Anton" || name == "Anton" && name2 == "Ilya")
+            //{
+            //    Console.WriteLine($"{name} and {name2} sit together");
+            //    string otvet3 = Console.ReadLine();
+            //}
+            //else if (name == "Denis" && name2 == "Artjom" || name == "Artjom" && name2 == "Denis")
+            //{
+            //    Console.WriteLine($"{name} and {name2} sit together");
+            //    string otvet4 = Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Neighbors is out");
+            //}
+            //int plosad = 0;
+            //Console.WriteLine("Какая длина стены a?");
+            //int dlina1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Какая длина стены b?");
+            //int dlina2 = int.Parse(Console.ReadLine());
+            //plosad = dlina1 * dlina2;
+            //Console.WriteLine($"Площадь пола = {plosad}");
+            //Console.WriteLine("Хотели бы вы сделать ремонт?");
+            //string remont = Console.ReadLine();
+            //if (remont.ToUpper() == "YES" || remont.ToUpper() == "ДА")
+            //{
+            //    Console.WriteLine("Сколько стоит квадратный метр?");
+            //    int odinmetr = int.Parse(Console.ReadLine());
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ну тогда до связи!");
+            //}
+
+            //Console.WriteLine("Сколько градусов в вашей комнате?");
+            //int answer = int.Parse(Console.ReadLine());
+            //if (answer >= 18)
+            //{
+            //    Console.WriteLine("Ваша температура в комнате выше 18 градусов");
+            //    string otvet = Console.ReadLine();
+
+
+
+        }
+    }
+}
