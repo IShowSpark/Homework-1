934fe102c391cf783f0b8a2c273ac56b48130df6
IShowSpark <93324182+IShowSpark@users.noreply.github.com>
2022-02-23 11:27:42 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,67 +6,109 @@ using System.Threading.Tasks;
 
 namespace Homework
 {
-    class Program
+    class classwork
     {
         static void Main(string[] args)
         {
-            //} while (k.Key!=ConsoleKey.Escape);
-            //Console.WriteLine("What your name?");
-            //string name = Console.ReadLine();
-            //Console.WriteLine("What name second person?");
-            //string name2 = Console.ReadLine();
-            //if (name == "Artjom" && name2 == "Timofei" || name == "Timofei" && name2 == "Artjom")
+            Random rnd = new Random();
+            //int value = rand.Next(1, 100);
+            //List<int> list = new List<int>();
+            //List<int> add = new List<int>();
+            //List<int> add1 = new List<int>();
+            //int a = 1;
+            //foreach (int item in list)
             //{
-            //    Console.WriteLine($"{name} and {name2} sit together");
-            //    string otvet1 = Console.ReadLine();
-            //}
-            //else if (name == "German" && name2 == "Kevin" || name == "Kevin" && name2 == "German")
-            //{
-            //    Console.WriteLine($"{name} and {name2} sit together");
-            //    string otvet2 = Console.ReadLine();
-            //}
-            //else if (name == "Ilya" && name2 == "Anton" || name == "Anton" && name2 == "Ilya")
-            //{
-            //    Console.WriteLine($"{name} and {name2} sit together");
-            //    string otvet3 = Console.ReadLine();
-            //}
-            //else if (name == "Denis" && name2 == "Artjom" || name == "Artjom" && name2 == "Denis")
-            //{
-            //    Console.WriteLine($"{name} and {name2} sit together");
-            //    string otvet4 = Console.ReadLine();
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Neighbors is out");
-            //}
-            //int plosad = 0;
-            //Console.WriteLine("Какая длина стены a?");
-            //int dlina1 = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Какая длина стены b?");
-            //int dlina2 = int.Parse(Console.ReadLine());
-            //plosad = dlina1 * dlina2;
-            //Console.WriteLine($"Площадь пола = {plosad}");
-            //Console.WriteLine("Хотели бы вы сделать ремонт?");
-            //string remont = Console.ReadLine();
-            //if (remont.ToUpper() == "YES" || remont.ToUpper() == "ДА")
-            //{
-            //    Console.WriteLine("Сколько стоит квадратный метр?");
-            //    int odinmetr = int.Parse(Console.ReadLine());
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Ну тогда до связи!");
+            //    if (item%2==0)
+            //    {
+            //        add.Add(item);
+            //        Console.WriteLine(item);
+            //    }
+            //    else
+            //    {
+            //        add1.Add(item);
+            //        Console.WriteLine(item);
+            //    }
+            //Console.ReadLine();
             //}
 
-            //Console.WriteLine("Сколько градусов в вашей комнате?");
-            //int answer = int.Parse(Console.ReadLine());
-            //if (answer >= 18)
-            //{
-            //    Console.WriteLine("Ваша температура в комнате выше 18 градусов");
-            //    string otvet = Console.ReadLine();
 
 
 
+
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            List<string> country = new List<string> { "Estonia", "Russia", "England", "Germany", "Finland" };
+            List<string> capital = new List<string> { "Tallinn", "Moscow", "London", "Berlin", "Helsinki" };
+            bool wantTo = true;
+            int randInt = 0;
+            double score = 0;
+
+            for (int i = 0; i < country.Count; i++)
+            {
+                dict.Add(country[i], capital[i]);
+                dict.Add(capital[i], country[i]);
+            }
+            while (wantTo == true)
+            {
+
+                Console.WriteLine("Search capital/country - 1, test - 2");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == 1)
+                {
+                    Console.Write("Entry capital/country: ");
+                    string input = Console.ReadLine();
+                    if (dict.ContainsKey(input))
+                    {
+                        Console.WriteLine("Pair of " + input + " is " + dict[input]);
+                    }
+                    else if (!dict.ContainsKey(input))
+                    {
+                        Console.WriteLine("do you want to add new words? yes - 1, no - 2");
+                        answer = int.Parse(Console.ReadLine());
+                        if (answer == 1)
+                        {
+                            Console.WriteLine("Enter new country please");
+                            string new1 = Console.ReadLine();
+
+                            Console.WriteLine("Enter new capital please");
+                            string new2 = Console.ReadLine();
+                            dict.Add(new1, new2);
+                            dict.Add(new2, new1);
+                        }
+                    }
+                }
+                else if (answer == 2)
+                {
+                    score = 0;
+                    for (int i = 0; i < country.Count; i++)
+                    {
+                        randInt = rnd.Next(1, 3);
+                        int b = rnd.Next(1, country.Count);
+                        if (randInt == 1)
+                        {
+                            Console.WriteLine("This is - " + country[b]);
+                            string userInput = Console.ReadLine();
+                            if (capital.IndexOf(userInput) == country.IndexOf(country[b]))
+                            {
+                                Console.WriteLine("Yes!");
+                                score++;
+                            }
+                        }
+                        else if (randInt == 2)
+                        {
+                            Console.WriteLine("This is - " + capital[b]);
+                            string userInput = Console.ReadLine();
+                            if (country.IndexOf(userInput) == capital.IndexOf(capital[b]))
+                            {
+                                Console.WriteLine("Yes!");
+                                score++;
+                            }
+                        }
+                    }
+                    Console.WriteLine(score / country.Count * 100 + "%");
+                }
+            }
         }
     }
 }
